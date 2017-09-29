29521ca0d59201c509297ddd0c5457dbd434a7c2
yooqin <yooqin@wow-china.com>
2017-09-29 14:16:02 +0800

    add backup 20170929141602

--- backup_development.sh
+++ backup_development.sh
@@ -6,18 +6,23 @@ echo "> begin backup"
 
 echo "- backup www dir";
 
+password=$1
 day=`date "+%Y%m%d%H%M%S"`
 
 filename="development-www-$day.tar.gz"
 
 echo $filename
 
-tar -czvf $filename $backdir
+tar -Cczvf $filename $backdir
 
-openssl aes-128-cbc -salt -k 123 -in $filename -out "$filename.aes"
+openssl aes-128-cbc -salt -k $password -in $filename -out "$filename.aes"
 
 rm $filename
 
 git add -A
 git commit -am "add backup $day"
 git push origin master
+
+
+
+#openssl aes-128-cbc -d -salt -k password -in development-www-20170929140740.tar.gz.aes -out out.tar.gz
