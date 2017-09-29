f29a52dd993e38314ea6aef7fe8e5b4f0289417b
yooqin <yooqin@wow-china.com>
2017-09-29 14:07:40 +0800

    add backup 20170929140740

--- backup_development.sh
+++ backup_development.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-backdir="~/www"
+backdir="/home/yooqin/www/testa"
 
 echo "> begin backup"
 
@@ -8,9 +8,16 @@ echo "- backup www dir";
 
 day=`date "+%Y%m%d%H%M%S"`
 
-filename="development-www-$day"
+filename="development-www-$day.tar.gz"
 
 echo $filename
 
 tar -czvf $filename $backdir
 
+openssl aes-128-cbc -salt -k 123 -in $filename -out "$filename.aes"
+
+rm $filename
+
+git add -A
+git commit -am "add backup $day"
+git push origin master
