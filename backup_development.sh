b5196f01d270e57603f8ab3dbbefb3e6d5d50894
yooqin <yooqin@wow-china.com>
2017-09-29 15:05:09 +0800

    add backup 20170929150501

--- backup_development.sh
+++ backup_development.sh
@@ -1,22 +1,37 @@
 #!/bin/bash
 
-backdir="/home/yooqin/www/testa"
-
-echo "> begin backup"
-
-echo "- backup www dir";
-
+backdir="/home/yooqin/www/mayflowes /home/yooqin/www/oa /home/yooqin/www/sports_lottery"
 password=$1
 day=`date "+%Y%m%d%H%M%S"`
-
 filename="development-www-$day.tar.gz"
+sqlfilename="/home/yooqin/hzcsDevelopmentBackup/development-mysql-$day.tar.gz"
+sqldir="/home/yooqin/sqlback"
 
-echo $filename
+if [ ! -n "$password" ] ;then
+    echo "use eg: backup_development.sh password "
+    exit;
+fi
 
+echo ">>>>> Script will run"
+
+#BackMysql
+
+cd /home/yooqin/hzcsDevelopmentBackup
+echo "-------Backup Mysql"
+rm $sqldir/*.dump
+
+mysqldump -uroot -p'e(YY33b..' sportslottery > $sqldir/sportslottery.dump
+mysqldump -uroot -p'e(YY33b..' oa > $sqldir/oa.dump
+mysqldump -uroot -p'e(YY33b..' mayflowes > $sqldir/mayflowes.dump
+
+tar -czvf $sqlfilename $sqldir
+openssl aes-128-cbc -salt -k $password -in $sqlfilename -out "$sqlfilename.aes"
+rm $sqlfilename
+#end BackMysql
+
+echo "-----Backup PHP"
 tar -czvf $filename $backdir
-
 openssl aes-128-cbc -salt -k $password -in $filename -out "$filename.aes"
-
 rm $filename
 
 git add -A
@@ -24,5 +39,4 @@ git commit -am "add backup $day"
 git push origin master
 
 
-
-#openssl aes-128-cbc -d -salt -k password -in development-www-20170929140740.tar.gz.aes -out out.tar.gz
+#openssl aes-128-cbc -d -salt -in development-www-20170929140740.tar.gz.aes -out out.tar.gz
